98de1c01aee229d90d611cb3239ce076e9d17a62
Phan Quang Minh Long <lordaizensousuke2001@gmail.com>
2022-09-13 00:04:46 +0700

    up

--- config.js
+++ config.js
@@ -1,19 +1,31 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
+    introTitle: 'CHÀO ĐẰNG ẤY!',
+    // introDesc: [`Ta chỉ là chiếc lá`,`
+    // Việc của mình là xanh.`,`
+    // Ta chỉ là quả chanh`,`
+    // Chờ một người đến vắt.`,`
+    // Ta chỉ là đôi mắt`,`
+    // Ngơ ngác nhìn đời trôi.`,`
+    // Ta chỉ là bờ môi`,`
+    // Muốn chạm vào ai quá!`],
+    introDesc: `Ta chỉ là chiếc lá
+    Việc của mình là xanh.
+    Ta chỉ là quả chanh
+    Chờ một người đến vắt.
+    Ta chỉ là đôi mắt
+    Ngơ ngác nhìn đời trôi.
+    Ta chỉ là bờ môi
+    Muốn chạm vào ai quá!`,
+    btnIntro: 'Ỏỏỏỏỏ 🥰🥰🥰',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
+    btnYes: 'Em thích anh <3 ♥️ 😘',
+    btnNo: 'KÓ :((( 😞💔',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh? 💞💓',
+    btnReply: 'Gửi anh 💘💌',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.facebook.com/pqmlong265' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messDesc: 'Ngày mai đi chơi với anh nhé công chúa đời anh.',
+    btnAccept: 'DẠẠẠẠẠ 💖💖',
+    messLink: 'https://www.facebook.com/pqmlong265'
 }
--- js/main.js
+++ js/main.js
@@ -22,7 +22,7 @@ function firstQuestion(){
     Swal.fire({
         title: CONFIG.introTitle,
         text: CONFIG.introDesc,
-        imageUrl: 'img/lookMe.jpg',
+        imageUrl: 'https://c.tenor.com/kIYoWB1BLfgAAAAC/love-cute.gif',
         imageWidth: 300,
         imageHeight: 300,
         background: '#fff url("img/iput-bg.jpg")',
@@ -103,7 +103,7 @@ $('#yes').click(function() {
         html: true,
         width: 900,
         padding: '3em',
-        html: "<input type='text' class='form-control' id='txtReason' onmousemove=textGenerate()  placeholder='Whyyy'>",
+        html: "<input type='text' class='form-control' id='txtReason' onmousemove=textGenerate()  placeholder='Sao vậy bé?'>",
         background: '#fff url("img/iput-bg.jpg")',
         backdrop: `
               rgba(0,0,123,0.4)
