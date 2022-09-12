d4a35703c8ecc6b64f94f84659a20189e99dc20a
Phan Quang Minh Long <lordaizensousuke2001@gmail.com>
2022-09-13 00:57:52 +0700

    phew

--- config.js
+++ config.js
@@ -1,31 +1,31 @@
-const CONFIG = {
-    introTitle: 'CHÀO ĐẰNG ẤY!',
-    // introDesc: [`Ta chỉ là chiếc lá`,`
-    // Việc của mình là xanh.`,`
-    // Ta chỉ là quả chanh`,`
-    // Chờ một người đến vắt.`,`
-    // Ta chỉ là đôi mắt`,`
-    // Ngơ ngác nhìn đời trôi.`,`
-    // Ta chỉ là bờ môi`,`
-    // Muốn chạm vào ai quá!`],
-    introDesc: `Ta chỉ là chiếc lá
-    Việc của mình là xanh.
-    Ta chỉ là quả chanh
-    Chờ một người đến vắt.
-    Ta chỉ là đôi mắt
-    Ngơ ngác nhìn đời trôi.
-    Ta chỉ là bờ môi
-    Muốn chạm vào ai quá!`,
-    btnIntro: 'Ỏỏỏỏỏ 🥰🥰🥰',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Em thích anh <3 ♥️ 😘',
-    btnNo: 'Hem biết :((( 😞💔',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh? 💞💓',
-    btnReply: 'Gửi anh 💘💌',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Ngày mai đi chơi với anh nhé công chúa đời anh.',
-    btnAccept: 'DẠẠẠẠẠ 💖💖',
-    messLink: 'https://www.facebook.com/pqmlong265'
-}
+// const CONFIG = {
+//     introTitle: 'CHÀO ĐẰNG ẤY!',
+//     // introDesc: [`Ta chỉ là chiếc lá`,`
+//     // Việc của mình là xanh.`,`
+//     // Ta chỉ là quả chanh`,`
+//     // Chờ một người đến vắt.`,`
+//     // Ta chỉ là đôi mắt`,`
+//     // Ngơ ngác nhìn đời trôi.`,`
+//     // Ta chỉ là bờ môi`,`
+//     // Muốn chạm vào ai quá!`],
+//     introDesc: `Ta chỉ là chiếc lá
+//     Việc của mình là xanh.
+//     Ta chỉ là quả chanh
+//     Chờ một người đến vắt.
+//     Ta chỉ là đôi mắt
+//     Ngơ ngác nhìn đời trôi.
+//     Ta chỉ là bờ môi
+//     Muốn chạm vào ai quá!`,
+//     btnIntro: 'Ỏỏỏỏỏ 🥰🥰🥰',
+//     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+//     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+//     btnYes: 'Em thích anh <3 ♥️ 😘',
+//     btnNo: 'Hem biết :((( 😞💔',
+//     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh? 💞💓',
+//     btnReply: 'Gửi anh 💘💌',
+//     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+//     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+//     messDesc: 'Ngày mai đi chơi với anh nhé công chúa đời anh.',
+//     btnAccept: 'DẠẠẠẠẠ 💖💖',
+//     messLink: 'https://www.facebook.com/pqmlong265'
+// }
---
+++ public/script.js
@@ -0,0 +1,165 @@
+const CONFIG = {
+    introTitle: 'CHÀO ĐẰNG ẤY!',
+    // introDesc: [`Ta chỉ là chiếc lá`,`
+    // Việc của mình là xanh.`,`
+    // Ta chỉ là quả chanh`,`
+    // Chờ một người đến vắt.`,`
+    // Ta chỉ là đôi mắt`,`
+    // Ngơ ngác nhìn đời trôi.`,`
+    // Ta chỉ là bờ môi`,`
+    // Muốn chạm vào ai quá!`],
+    introDesc: `Ta chỉ là chiếc lá
+    Việc của mình là xanh.
+    Ta chỉ là quả chanh
+    Chờ một người đến vắt.
+    Ta chỉ là đôi mắt
+    Ngơ ngác nhìn đời trôi.
+    Ta chỉ là bờ môi
+    Muốn chạm vào ai quá!`,
+    btnIntro: 'Ỏỏỏỏỏ 🥰🥰🥰',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Em thích anh <3 ♥️ 😘',
+    btnNo: 'Hem biết :((( 😞💔',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh? 💞💓',
+    btnReply: 'Gửi anh 💘💌',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Ngày mai đi chơi với anh nhé công chúa đời anh.',
+    btnAccept: 'DẠẠẠẠẠ 💖💖',
+    messLink: 'https://www.facebook.com/pqmlong265'
+}
+
+$(document).ready(function() {
+    // process bar
+    setTimeout(function() {
+        firstQuestion();
+        $('.spinner').fadeOut();
+        $('#preloader').delay(350).fadeOut('slow');
+        $('body').delay(350).css({
+            'overflow': 'visible'
+        });
+    }, 600);
+})
+
+function init(){
+    $('#title').text(CONFIG.title)
+    $('#desc').text(CONFIG.desc)
+    $('#yes').text(CONFIG.btnYes)
+    $('#no').text(CONFIG.btnNo)
+}
+
+function firstQuestion(){
+    $('.content').hide();
+    Swal.fire({
+        title: CONFIG.introTitle,
+        text: CONFIG.introDesc,
+        imageUrl: 'https://c.tenor.com/kIYoWB1BLfgAAAAC/love-cute.gif',
+        imageWidth: 300,
+        imageHeight: 300,
+        background: '#fff url("img/iput-bg.jpg")',
+        imageAlt: 'Custom image',
+        confirmButtonText: CONFIG.btnIntro
+      }).then(function(){
+        $('.content').show(200);
+      })
+}
+
+ // switch button position
+ function switchButton() {
+    var audio = new Audio('sound/duck.mp3');
+    audio.play();
+    var leftNo = $('#no').css("left");
+    var topNO = $('#no').css("top");
+    var leftY = $('#yes').css("left");
+    var topY = $('#yes').css("top");
+    $('#no').css("left", leftY);
+    $('#no').css("top", topY);
+    $('#yes').css("left", leftNo);
+    $('#yes').css("top", topNO);
+}
+// move random button póition
+function moveButton() {
+    var audio = new Audio('sound/Swish1.mp3');
+    audio.play();
+    var x = Math.random() * ($(window).width() - $('#no').width()) * 0.9 ;
+    var y = Math.random() * ($(window).height() - $('#no').height()) * 0.9;
+    var left = x + 'px';
+    var top = y + 'px';
+    $('#no').css("left", left);
+    $('#no').css("top", top);
+}
+
+init()
+
+var n = 0;
+$('#no').mousemove(function() {
+    if (n < 1)
+        switchButton();
+    if (n > 1)
+        moveButton();
+    n++;
+});
+$('#no').click(() => {
+    if (screen.width>=900)
+        switchButton();
+})
+
+// generate text in input
+function textGenerate() {
+    var n = "";
+    var text = " " + CONFIG.reply;
+    var a = Array.from(text);
+    var textVal = $('#txtReason').val() ? $('#txtReason').val() : "";
+    var count = textVal.length;
+    if (count > 0) {
+        for (let i = 1; i <= count; i++) {
+            n = n + a[i];
+            if (i == text.length + 1) {
+                $('#txtReason').val("");
+                n = "";
+                break;
+            }
+        }
+    }
+    $('#txtReason').val(n);
+    setTimeout("textGenerate()", 1);
+}
+
+// show popup
+$('#yes').click(function() {
+    var audio = new Audio('sound/tick.mp3');
+    audio.play();
+    Swal.fire({
+        title: CONFIG.question,
+        html: true,
+        width: 900,
+        padding: '3em',
+        html: "<input type='text' class='form-control' id='txtReason' onmousemove=textGenerate()  placeholder='Sao vậy bé?'>",
+        background: '#fff url("img/iput-bg.jpg")',
+        backdrop: `
+              rgba(0,0,123,0.4)
+              url("img/giphy2.gif")
+              left top
+              no-repeat
+            `,
+        confirmButtonColor: '#3085d6',
+        confirmButtonColor: '#fe8a71',
+        confirmButtonText: CONFIG.btnReply
+    }).then((result) => {
+        if (result.value) {
+            Swal.fire({
+                width: 900,
+                confirmButtonText: CONFIG.btnAccept,
+                background: '#fff url("img/iput-bg.jpg")',
+                title: CONFIG.mess,
+                text: CONFIG.messDesc,
+                confirmButtonColor: '#83d0c9',
+                onClose: () => {
+                    window.location = CONFIG.messLink;
+                  }
+            })
+        }
+    })
+})
+
